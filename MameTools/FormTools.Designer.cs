684b8fd3f6169ca95cf9dbfe851f845de5e52623
Fernando Lorenzon <Fernando Lorenzon>
2020-07-15 11:51:05 -0300

    Add project files.

---
+++ MameTools/FormTools.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace MameTools
+{
+    partial class FormTools
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTools));
+            this.buttonMoveFound = new System.Windows.Forms.Button();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDestination = new System.Windows.Forms.TextBox();
+            this.buttonMoveNotFound = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButtonSkipExt = new System.Windows.Forms.RadioButton();
+            this.radioButtonFullName = new System.Windows.Forms.RadioButton();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonListFolder = new System.Windows.Forms.Button();
+            this.buttonSource = new System.Windows.Forms.Button();
+            this.buttonDest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMoveFound
+            // 
+            this.buttonMoveFound.Location = new System.Drawing.Point(12, 401);
+            this.buttonMoveFound.Name = "buttonMoveFound";
+            this.buttonMoveFound.Size = new System.Drawing.Size(135, 80);
+            this.buttonMoveFound.TabIndex = 0;
+            this.buttonMoveFound.Text = "Move found";
+            this.buttonMoveFound.UseVisualStyleBackColor = true;
+            this.buttonMoveFound.Click += new System.EventHandler(this.buttonMoveFound_Click);
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Location = new System.Drawing.Point(12, 52);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(412, 20);
+            this.textBoxSource.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source Roms folder:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Destination Roms folder:";
+            // 
+            // textBoxDestination
+            // 
+            this.textBoxDestination.Location = new System.Drawing.Point(12, 94);
+            this.textBoxDestination.Name = "textBoxDestination";
+            this.textBoxDestination.Size = new System.Drawing.Size(412, 20);
+            this.textBoxDestination.TabIndex = 3;
+            // 
+            // buttonMoveNotFound
+            // 
+            this.buttonMoveNotFound.Location = new System.Drawing.Point(153, 401);
+            this.buttonMoveNotFound.Name = "buttonMoveNotFound";
+            this.buttonMoveNotFound.Size = new System.Drawing.Size(135, 80);
+            this.buttonMoveNotFound.TabIndex = 5;
+            this.buttonMoveNotFound.Text = "Move not found";
+            this.buttonMoveNotFound.UseVisualStyleBackColor = true;
+            this.buttonMoveNotFound.Click += new System.EventHandler(this.buttonMoveNotFound_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Rom list: (separate by line)";
+            // 
+            // radioButtonSkipExt
+            // 
+            this.radioButtonSkipExt.AutoSize = true;
+            this.radioButtonSkipExt.Checked = true;
+            this.radioButtonSkipExt.Location = new System.Drawing.Point(430, 134);
+            this.radioButtonSkipExt.Name = "radioButtonSkipExt";
+            this.radioButtonSkipExt.Size = new System.Drawing.Size(95, 17);
+            this.radioButtonSkipExt.TabIndex = 8;
+            this.radioButtonSkipExt.TabStop = true;
+            this.radioButtonSkipExt.Text = "Skip Extension";
+            this.radioButtonSkipExt.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFullName
+            // 
+            this.radioButtonFullName.AutoSize = true;
+            this.radioButtonFullName.Location = new System.Drawing.Point(430, 157);
+            this.radioButtonFullName.Name = "radioButtonFullName";
+            this.radioButtonFullName.Size = new System.Drawing.Size(72, 17);
+            this.radioButtonFullName.TabIndex = 9;
+            this.radioButtonFullName.Text = "Full Name";
+            this.radioButtonFullName.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 134);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(412, 261);
+            this.richTextBox.TabIndex = 11;
+            this.richTextBox.Text = "";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(386, 398);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(13, 13);
+            this.labelCount.TabIndex = 12;
+            this.labelCount.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(342, 398);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Count:";
+            // 
+            // buttonListFolder
+            // 
+            this.buttonListFolder.Location = new System.Drawing.Point(430, 222);
+            this.buttonListFolder.Name = "buttonListFolder";
+            this.buttonListFolder.Size = new System.Drawing.Size(135, 80);
+            this.buttonListFolder.TabIndex = 14;
+            this.buttonListFolder.Text = "<< List a folder";
+            this.buttonListFolder.UseVisualStyleBackColor = true;
+            this.buttonListFolder.Click += new System.EventHandler(this.buttonListFolder_Click);
+            // 
+            // buttonSource
+            // 
+            this.buttonSource.Location = new System.Drawing.Point(430, 52);
+            this.buttonSource.Name = "buttonSource";
+            this.buttonSource.Size = new System.Drawing.Size(78, 20);
+            this.buttonSource.TabIndex = 15;
+            this.buttonSource.Text = "...";
+            this.buttonSource.UseVisualStyleBackColor = true;
+            this.buttonSource.Click += new System.EventHandler(this.buttonSource_Click);
+            // 
+            // buttonDest
+            // 
+            this.buttonDest.Location = new System.Drawing.Point(430, 93);
+            this.buttonDest.Name = "buttonDest";
+            this.buttonDest.Size = new System.Drawing.Size(78, 20);
+            this.buttonDest.TabIndex = 16;
+            this.buttonDest.Text = "...";
+            this.buttonDest.UseVisualStyleBackColor = true;
+            this.buttonDest.Click += new System.EventHandler(this.buttonDest_Click);
+            // 
+            // FormTools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 493);
+            this.Controls.Add(this.buttonDest);
+            this.Controls.Add(this.buttonSource);
+            this.Controls.Add(this.buttonListFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.radioButtonFullName);
+            this.Controls.Add(this.radioButtonSkipExt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonMoveNotFound);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxDestination);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSource);
+            this.Controls.Add(this.buttonMoveFound);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormTools";
+            this.Text = "MAME Tools";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMoveFound;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDestination;
+        private System.Windows.Forms.Button buttonMoveNotFound;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButtonSkipExt;
+        private System.Windows.Forms.RadioButton radioButtonFullName;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonListFolder;
+        private System.Windows.Forms.Button buttonSource;
+        private System.Windows.Forms.Button buttonDest;
+    }
+}
+
---
+++ MameTools/FormTools.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MameTools
+{
+    public partial class FormTools : Form
+    {
+        public FormTools()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMoveFound_Click(object sender, EventArgs e)
+        {
+            Move(true);
+        }
+
+        private void buttonMoveNotFound_Click(object sender, EventArgs e)
+        {
+            Move(false);
+        }
+
+        private void Move(bool movefound)
+        {
+            labelCount.Text = "0";
+            if (textBoxSource.Text == "")
+            {
+                MessageBox.Show("Select source folder");
+                return;
+            }
+
+            if (textBoxDestination.Text == "")
+            {
+                MessageBox.Show("Select destination folder");
+                return;
+            }
+
+            if (richTextBox.Text == "")
+            {
+                MessageBox.Show("Insert list");
+                return;
+            }
+
+            if (!Directory.Exists(textBoxSource.Text)) return;
+            if (!Directory.Exists(textBoxDestination.Text)) return;
+
+            string separator = richTextBox.Text.IndexOf("\r\n") > -1 ? "\r\n" : "\n";
+
+            var files = Directory.GetFiles(textBoxSource.Text, "*.*", SearchOption.TopDirectoryOnly).ToList();
+            var list = richTextBox.Text.Split(new string[] { separator }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            string file = "";
+
+            int count = 0;
+
+            foreach (var path in files)
+            {
+                if (radioButtonFullName.Checked)
+                {
+                    file = GetFileName(path);
+                }
+                else
+                {
+                    file = GetFileNameNoExtension(path);
+                }
+
+                if (movefound)
+                {
+                    if (list.Any(x => x == file))
+                    {
+                        if (radioButtonFullName.Checked)
+                        {
+                            File.Move(path, textBoxDestination.Text + "\\" + file);
+                        }
+                        else
+                        {
+                            File.Move(path, textBoxDestination.Text + "\\" + file + GetFileExtension(path));
+                        }
+
+                        count++;
+                    }
+                }
+                else
+                {
+                    if (!list.Any(x => x == file))
+                    {
+                        if (radioButtonFullName.Checked)
+                        {
+                            File.Move(path, textBoxDestination.Text + "\\" + file);
+                        }
+                        else
+                        {
+                            File.Move(path, textBoxDestination.Text + "\\" + file + GetFileExtension(path));
+                        }
+
+                        count++;
+                    }
+                }
+
+                labelCount.Text = count.ToString();
+                labelCount.Refresh();
+            }
+
+            MessageBox.Show("Success!");
+        }
+
+        public static string GetFileExtension(string file)
+        {
+            if (string.IsNullOrEmpty(file)) return "";
+            return file.Substring(file.LastIndexOf("."));
+        }
+
+        public static string GetFileName(string file)
+        {
+            if (string.IsNullOrEmpty(file)) return "";
+            return file.Substring(file.LastIndexOf("\\") + 1);
+        }
+
+        public static string GetFileNameNoExtension(string file)
+        {
+            if (string.IsNullOrEmpty(file)) return "";
+            var fileName = file.Substring(file.LastIndexOf("\\") + 1);
+            var indexExtension = fileName.LastIndexOf(".");
+
+            if (indexExtension == -1)
+            {
+                return fileName;
+            }
+            else
+            {
+                return fileName.Substring(0, fileName.LastIndexOf("."));
+            }
+        }
+
+        private void buttonListFolder_Click(object sender, EventArgs e)
+        {
+            richTextBox.Text = "";
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+            var result = dialog.ShowDialog();
+
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            var files = Directory.GetFiles(dialog.SelectedPath);
+
+            foreach (var file in files)
+            {
+                richTextBox.Text += GetFileNameNoExtension(file) + "\r\n";
+            }
+        }
+
+        private void buttonSource_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+            var result = dialog.ShowDialog();
+
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            textBoxSource.Text = dialog.SelectedPath;
+        }
+
+        private void buttonDest_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+            var result = dialog.ShowDialog();
+
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            textBoxDestination.Text = dialog.SelectedPath;
+        }
+    }
+}
